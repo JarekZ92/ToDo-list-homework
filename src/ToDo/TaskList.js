cf9eb648cf4c8a793fb70c684eec508002d05e3f
JarekZ92 <z.jarek92@gmail.com>
2018-08-04 18:09:46 +0200

    moved PaperRefined

--- src/ToDo/TaskList.js
+++ src/ToDo/TaskList.js
@@ -1,4 +1,20 @@
 import React from 'react'
 
 import Task from './Task'
-import PaperRefined from './PaperRefined'
+import PaperRefined from './../PaperRefined'
+
+const TaskList = (props) => (
+    <PaperRefined>
+        {
+            props.taskList.map(
+                task => (
+                    <Task
+                    key={task.key}
+                    task={task}
+                )
+            )
+        }
+        </PaperRefined>
+    )
+
+    export default TaskList
