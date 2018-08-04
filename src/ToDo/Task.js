e1fed85f459bce0c292dcbeb20af836cb5362cb9
JarekZ92 <z.jarek92@gmail.com>
2018-08-04 16:24:00 +0200

    added Task component

---
+++ src/ToDo/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import {ListItem} from 'material-ui/List'
+
+const Task = (props) => {
+    <ListItem>
+        {props.task.text}
+        </ListItem>
+}
+
+export default Task
